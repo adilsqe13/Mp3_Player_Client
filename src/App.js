44ad5fa6079309e845ec01faab03de5b5791aa30
adilsqe13 <adilsqe13@gmail.com>
2024-02-03 13:17:58 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
     setFileName(file.name);
   };
 
+  //GET all the Audios from the backend server
   const getAllAudios = async () => {
     const response = await fetch(`${apiUrl}/api/get-all-audios`, {
       method: 'GET',
@@ -33,6 +34,7 @@ function App() {
     setAudios(audios.reverse());
   }
 
+  // Upload Audio on Cloudinary
   const uploadAudio = async () => {
     setProcessing(true);
     if (audioFile) {
@@ -52,37 +54,43 @@ function App() {
         }
       );
 
-      await axios.post(
-        `${apiUrl}/api/upload-audio`,
-        {
-          audioUrl: await response.data.secure_url,
-          public_id: await response.data.public_id,
-          fileName: fileName,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
+      // POST data to the server
+      try {
+        await axios.post(
+          `${apiUrl}/api/upload-audio`,
+          {
+            audioUrl: await response.data.secure_url,
+            public_id: await response.data.public_id,
+            fileName: fileName,
           },
-        }
-      )
-        .then(response => {
-          if (response.data.success) {
-            window.location.reload();
-            localStorage.setItem('currentAudioIndex', currentAudioIndex + 1)
-            setProcessing(false);
-          } else {
-            setProcessing(false);
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
           }
-        })
-        .catch(error => {
-          console.log(error);
-          setProcessing(false);
-        })
+        )
+          .then(response => {
+            if (response.data.success) {
+              window.location.reload();
+              localStorage.setItem('currentAudioIndex', 0);
+              setProcessing(false);
+            } else {
+              setProcessing(false);
+            }
+          })
+          .catch(error => {
+            console.log(error);
+            setProcessing(false);
+          })
+      } catch (error) {
+        console.log(error);
+      }
     } else {
       console.error('No file selected');
     }
   }
 
+  // Play next audio after one completed
   const playNextAudio = async (index) => {
     if (index !== await audios.length) {
       document.getElementById(index).play();
@@ -91,13 +99,17 @@ function App() {
       console.log('Playlist songs ended');
     }
   };
+
+  // Update Playing Audio timeing
   const handleTimeUpdate = (audioElement) => {
     localStorage.setItem('currentPlaybackPosition', audioElement.currentTime);
   };
 
+
+  // Play Audio by clicking
   const handlePlay = async (index) => {
     setCurrentAudioIndex(index);
-    if (index !== localStorage.getItem('currentAudioIndex')) {
+    if (index.toString() !== localStorage.getItem('currentAudioIndex')) {
       localStorage.removeItem('currentPlaybackPosition');
     }
     localStorage.setItem('currentAudioIndex', index);
@@ -114,14 +126,16 @@ function App() {
     }
   }
 
-  useEffect(() => {
-    getAllAudios();
-  }, []);
+ // eslint-disable-next-line react-hooks/exhaustive-deps
+useEffect(() => {
+  getAllAudios();
+}, []);
+
 
   return (
     <>
       <div className="container min-w-350 margin-top-100 bg-light rounded-4 px-0 pb-5">
-        <h1 className='dfjcac bg-danger text-light py-3 btlr-4 btrr-4'>Mp3 Audio Player <span>&nbsp; <FontAwesomeIcon style={{color:'#edda07'}} icon={faCompactDisc} /></span></h1>
+        <h1 className='dfjcac bg-danger text-light py-3 btlr-4 btrr-4 protest-guerrilla-regular'>Mp3 Audio Player <span>&nbsp; <FontAwesomeIcon style={{ color: '#edda07' }} icon={faCompactDisc} /></span></h1>
         <div className="row p-3 rounded-2 mb-2 w-75 margin-auto border bg-light border cfubb">
           <div className="col-6 dfjlac">
             <input type="file" id="upload" title="Upload File" onChange={handleFileChange} />
@@ -133,7 +147,7 @@ function App() {
             </button>
           </div>
         </div>
-        <div className="row mb-4 py-2 w-100 px-3 margin-auto bold fs-3">Playlist</div>
+        <div className="row mb-4 py-2 w-100 px-3 margin-auto bold fs-3 protest-guerrilla-regular"> <span>Playlist &nbsp; <small className='fs-6 fonts'>{audios !== null ? audios.length : 0} - audios</small></span></div>
         {audios == null && <div className='container dfjcac py-3'><Spinner height='70' width='70' /></div>}
         {audios !== null && (audios.length !== 0 ? audios.map((audio, index) => {
           return (
@@ -141,12 +155,12 @@ function App() {
               <div className="col-lg-6 dfjlac">
                 <div className="row">
                   <div className="col-1">
-                  <FontAwesomeIcon style={{color:`${index.toString() === localStorage.getItem('currentAudioIndex')? '#edda07': ''}`}} icon={faCompactDisc} />
+                    <FontAwesomeIcon style={{ color: `${index.toString() === localStorage.getItem('currentAudioIndex') ? '#edda07' : ''}` }} icon={faCompactDisc} />
                   </div>
                   <div className="col-11"><span className='bold'>{audio.fileName}</span></div>
                 </div>
-                </div>
-              <div className={`col-lg-6 rounded-4 ${index.toString() === localStorage.getItem('currentAudioIndex')? 'row-box-shadow': ''}`}>
+              </div>
+              <div className={`col-lg-6 rounded-4 ${index.toString() === localStorage.getItem('currentAudioIndex') ? 'row-box-shadow' : ''}`}>
                 <div className='dfjcac my-1'>
                   <audio className='audio-player' controls id={`${index}`} onPlay={() => { handlePlay(index) }}>
                     <source src={audio.audioUrl} type="audio/mp3" />
@@ -162,8 +176,8 @@ function App() {
           </div>)}
       </div>
       <div className="container">
-            <h6 className='mt-2 dfjcac text-secondary'>Developed by MD ADIL ALAM</h6>
-          </div>
+        <h6 className='mt-2 dfjcac text-secondary'>Developed by MD ADIL ALAM&nbsp;&nbsp;||&nbsp;&nbsp;Founder of &nbsp;<a rel="noreferrer" href='https://myshopper-ff0dc.web.app/' target='_blank'>mY Shopper</a> &nbsp;&nbsp;||&nbsp;&nbsp;Founder of &nbsp;<a rel="noreferrer" href='https://easytweet-d58c4.web.app/' target='_blank'>Tweeter-Clone</a></h6>
+      </div>
 
     </>
   );
